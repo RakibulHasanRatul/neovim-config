ff4404145e15b9ca33e04adb9a1d869b7c6eeac9
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2025-10-27 03:29:50 +0600

    fix: requiring lazy
    previously, require('lazy') at welcome-screen.lua file was trying to
    require lua/config/lazy.lua file! the file is renamed for that.

---
+++ lua/config/lazy-config.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	print("Installing lazy.nvim...")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+	print("lazy.nvim installed!")
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins will be loaded from lua/plugins/ directory
+require("lua.config.lazy-config").setup("plugins", {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
