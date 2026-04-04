505ba98b615fe56824fb11e767b25a2925d870f6
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-04-05 05:38:45 +0600

    fix: fix copying

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 rm -rf ~/.config/nvim ~/.local/share/nvim
 mkdir -p ~/.config/nvim
-cp -rfv ./* ~/.config/nvim
+tar -C $pwd --exclude '.git' \
+	--exclude install.sh \
+	--exclude README \
+	--exclude preview.png \
+	--exclude LICENSE.md \
+	-cf - . | tar -C ~/.config/nvim -xfv -
 echo "Installed nvim config to ~/.config/nvim"
--- lua/plugins/syntax-highlighting.lua
+++ lua/plugins/syntax-highlighting.lua
@@ -25,11 +25,27 @@ return {
 			"tsx",
 		})
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "<filetype>" },
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "<filetype>" },
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+
+        -- Enable incremental selection with Treesitter
+        -- Press <Enter> in Normal/Visual mode to start/expand selection progressively
+        local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
+        if ok and ts_configs then
+            ts_configs.setup({
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",        -- Start selection
+                        node_incremental = "<CR>",      -- Expand to next node
+                        node_decremental = "<BS>",       -- Shrink (backspace)
+                    },
+                },
+            })
+        end
 	end,
 }
