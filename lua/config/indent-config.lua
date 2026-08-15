7c88f5dd4435e2bcef9fadefc5573d5c9b44482c
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-08-15 20:46:45 +0600

    feat: enforce 2 space indentation for c/c++ files

--- lua/config/indent-config.lua
+++ lua/config/indent-config.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "lua", "tsx", "jsx" },
+  pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "lua", "tsx", "jsx", "c", "cpp", "cxx", "h", "hpp", "hxx", "cuda", "objc", "objcpp" },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
