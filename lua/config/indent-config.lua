002dadfd0dfd58d8cf8c542e48101eef97bac890
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-08-15 20:57:09 +0600

    chore: format file

--- lua/config/indent-config.lua
+++ lua/config/indent-config.lua
@@ -1,8 +1,25 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "lua", "tsx", "jsx", "c", "cpp", "cxx", "h", "hpp", "hxx", "cuda", "objc", "objcpp" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.expandtab = true
-  end,
+	pattern = {
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"javascriptreact",
+		"lua",
+		"tsx",
+		"jsx",
+		"c",
+		"cpp",
+		"cxx",
+		"h",
+		"hpp",
+		"hxx",
+		"cuda",
+		"objc",
+		"objcpp",
+	},
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end,
 })
