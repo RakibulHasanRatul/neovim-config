b5e355cab90bca56ef3788db62fb9cb36173b479
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-08-22 22:44:36 +0600

    feat: enable ctrl+q to send all to quick fix list for picker

--- lua/plugins/fuzzy-finder.lua
+++ lua/plugins/fuzzy-finder.lua
@@ -4,15 +4,16 @@ return {
 		"folke/snacks.nvim",
 		---@type snacks.Config
 		opts = {
-			picker = {
-				win = {
-					input = {
-						keys = {
-							["<Esc>"] = { "close", mode = { "n", "i" } },
-						},
+		picker = {
+			win = {
+				input = {
+					keys = {
+						["<Esc>"] = { "close", mode = { "n", "i" } },
+						["<C-q>"] = { "qflist_all", mode = { "n", "i" }, desc = "Send all to quickfix" },
 					},
 				},
 			},
+		},
 		},
 		keys = {
 			{
